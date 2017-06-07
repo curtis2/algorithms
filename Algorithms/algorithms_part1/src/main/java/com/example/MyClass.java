0b3cd5cae7026b3d64a339978f15f4962439f2ea
curtis2 <18292967668@163.com>
2017-06-07 17:35:32 +0800

    first commit

---
+++ Algorithms/algorithms_part1/src/main/java/com/example/MyClass.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String args[]) {
+
+//        int a=(0+15)/2;
+//        System.out.println(2.0e-6*100000000.1);
+        System.out.println(true&&false||true&&true);
+    }
+}
